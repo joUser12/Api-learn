c8005943f55a521e8763d1cef2d938fb75d5e4a9
joy s <joysundaran15@gmail.com>
2022-12-26 22:02:50 +0530

    section 3 changes

--- learnAPI Udemy/learnAPI/Program.cs
+++ learnAPI Udemy/learnAPI/Program.cs
@@ -1,4 +1,5 @@
 using learnAPI.Data;
+using learnAPI.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,10 @@ builder.Services.AddDbContext<NzWalksDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
 });
+
+builder.Services.AddScoped<IRegionRepository , RegionRepository>();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
---
+++ learnAPI Udemy/learnAPI/Repositories/RegionRepository.cs
@@ -0,0 +1,21 @@
+﻿using learnAPI.Data;
+using learnAPI.Models.Domain;
+
+namespace learnAPI.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NzWalksDbContext nzWalksDbContext;
+
+        public RegionRepository(NzWalksDbContext nzWalksDbContext)
+        {
+            this.nzWalksDbContext = nzWalksDbContext;
+        }
+
+        public  IEnumerable<Region> GetAll()
+        {
+
+          return  nzWalksDbContext.Regions.ToList();
+        }
+    }
+}
